2bd1d055e6c10222ed6d502d6676fb72bdf98818
Suzan <sys612603@gmail.com>
2023-09-30 02:44:51 +0300

    Add some server files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+import AuthRoute from "./Routes/AuthRoute.js";
 const app = express();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+dotenv.config();
+//Usage of Routes
+app.use("/auth", AuthRoute);
+
+mongoose
+  .connect(
+    "mongodb+srv://user2000:test234@cluster0.0jwhkdk.mongodb.net/socialappkiwi?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() =>
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening at port ${process.env.PORT}  `);
+    })
+  )
+  .catch((error) => {
+    console.log(error);
+  });
