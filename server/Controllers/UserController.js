2291020afc7ddbcf38496b8c387f596122dc2b20
Suzan <sys612603@gmail.com>
2023-10-04 02:25:50 +0300

    Get all users

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,1 +1,59 @@
-import UserModel from "../Models/userModel";
+import UserModel from "../Models/userModel.js";
+//Get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    let users = await UserModel.find();
+    users = users.map((user) => {
+      const { password, ...otherDetails } = user._doc;
+      return otherDetails;
+    });
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+//get a User
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await UserModel.findById(id);
+    if (user) {
+      const { password, ...otherDetails } = user._doc;
+      res.status(200).json(otherDetails);
+    } else {
+      res.status(404).json("No such user exists");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+//update a user
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { _id, currentUserAdmin, password } = req.body;
+  if (id === _id) {
+    try {
+      if (password) {
+        // const salt = await bcrypt.genSalt(10);
+        // req.body.password = await bcrypt.hash(password, salt);
+      }
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      //   const token = jwt.sign(
+      //     { username: user.username, id: user._id },
+      //     process.env.JWT_KEY,
+      //     { expiresIn: "1h" }
+      //   );
+      //   console.log({ user, token });
+      res.status(200).json(user);
+    } catch (error) {
+      console.log("Error agya hy");
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("Access Denied! you can only update your own profile");
+  }
+};
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,3 +1,11 @@
 import express from "express";
+import {
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../Controllers/UserController.js";
 const router = express.Router();
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
 export default router;
