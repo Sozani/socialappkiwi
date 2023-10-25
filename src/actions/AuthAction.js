e86b6e77de4774b4ff684b6eebcc508ebe2edd57
Suzan <sys612603@gmail.com>
2023-10-25 05:35:56 +0300

    login Action

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -1,3 +1,11 @@
+import * as AuthApi from "../api/AuthRequest";
 export const logIn = (formData) => async (dispatch) => {
-  const { data } = await AuthApi.logIn(formData);
+  dispatch({ type: "AUTH_START" });
+  try {
+    const { data } = await AuthApi.logIn(formData);
+    dispatch({ type: "AUTH_SUCCESS", data: data });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: "AUTH_FAIL" });
+  }
 };
--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,2 +1,3 @@
 import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
+export const logIn = (formData) => API.post("/auth/login", formData);
